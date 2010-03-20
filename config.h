ca78a8e8f79c1a87e79a1a698bf83b1c64c777a2
Steve Pinkham <steve.pinkham@gmail.com>
2010-03-20 11:51:34 -0400

    1.03b - Workaround for *BSD systems with malloc J or Z options set by default. - again

--- config.h
+++ config.h
@@ -23,7 +23,7 @@
 #ifndef _HAVE_CONFIG_H
 #define _HAVE_CONFIG_H
 
-#define VERSION "1.02b"
+#define VERSION "1.03b"
 
 #define USE_COLOR               1       /* Use terminal colors             */
 
--- skipfish.c
+++ skipfish.c
@@ -49,7 +49,7 @@ u32 __AD_trk_cnt[ALLOC_BUCKETS];
 
 
 /* *BSD where J or Z is set are incompatible with our allocator. */
-char* _malloc_options = "jz";
+char* malloc_options = "jz";
 
 
 void usage(char* argv0) {
